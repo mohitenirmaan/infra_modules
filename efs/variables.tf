eff8d707fb088eaed00c775b047b59f954450552
mohitenirmaan <mohit@enirmaan.com>
2024-06-20 11:37:54 +0530

    Update variables.tf

--- efs/variables.tf
+++ efs/variables.tf
@@ -1,1 +1,28 @@
+variable "private_subnet_ids" {
+ type = list(string) 
+}
+variable "security_group_ids" {
+ type = list(string) 
+}
 
+variable "name" {
+  description = "Name that will include to every resources"
+  type = string
+}
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}
+
+variable "creation_token" {
+  type = string
+}
+variable "encrypted" {
+  type = bool
+}
+variable "throughput_mode" {
+  type = string
+}
+variable "transition_to_ia" {
+  type = string
+}
