4a3910e26fe7d1a646ececec8afa576e57307200
Mohit Sharma <mohit@enirmaan.com>
2024-06-25 15:44:42 +0530

    added rds module

---
+++ rds/variables.tf
@@ -0,0 +1,28 @@
+
+variable "name" {
+  description = "Name that will include to every resources"
+  type = string
+}
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+}
+variable "vpc_id" {
+  type = string
+}
+variable "private_subnet_ids" {
+ type = list(string) 
+}
+variable "rds_sg_name" {
+  type = string
+}
+# variable "inress-cidr_blocks" {
+#  type = list(string)
+# }
+variable "db_subnet_group_name" {
+  type = string
+}
+variable "db_instance_config" {
+  description = "Configuration map for the RDS instance."
+  type        = map(any)
+}
